16b368b99f5f90d8755932b446abfaa0c310dd0a
Hottensiah Wanjiku <w.hottensiah96@gmail.com>
2024-07-04 00:44:08 +0300

    first commit

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_sequence = []
+    
+    if length > 0:
+        fibonacci_sequence = [0]
+    
+    if length > 1:
+        fibonacci_sequence.append(1)
+    
+    while len(fibonacci_sequence) < length:
+        next_num = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        fibonacci_sequence.append(next_num)
+    
+    print(fibonacci_sequence)
+
+
+
+     
